93e99f438b4229bcf2e97047f512971c5bd89222
MaxabHaase <maxabhaase@gmail.com>
2017-09-15 15:14:03 -0400

    Update

--- week_01/9_15_17/Calculator_mine.py
+++ week_01/9_15_17/Calculator_mine.py
@@ -1,8 +1,8 @@
-#input values from user
+# input values from user
 value_1 = input("Enter your first value")
 value_2 = input("Enter your second value")
 operator = input("Enter the operator")
-#validate inputs
+# validate inputs
 valid_values = True
 valid_operator = True
 if value_1.isdigit() and value_2.isdigit():
@@ -14,7 +14,7 @@ else:
 if operator not in '+-/*':
     valid_operator = False
     print("The operator is False")
-#Calculate
+# Calculate
 if valid_operator and valid_values == True:
     if operator == '+':
         print("%d + %d = " % (value_1, value_2), value_1 + value_2)
